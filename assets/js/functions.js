5f1091310da35a95e6103a50a7a5d688e6803fa0
Termins55 <kevgame123@gmail.com>
2025-06-24 16:58:29 +0300

    Add functions default value, JSDoc

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -90,9 +90,99 @@
 // const ourAge2 = age2(18);
 // console.log(ourAge2);
 
-function isAdult(age) {
-    return age >= 18;
+// function isAdult(age) {
+//     return age >= 18;
+// }
+
+// console.log('isAdult :>> ', isAdult(2));
+
+// Способи задання функцій
+// Function Declaration / Оголошення функції
+// function fName(param1, param2, ...){
+//   тіло функції
+//   return значення;
+// }
+
+// Function Expression / Вираз функції
+// const fName = function (param1, param2, ...){
+//   тіло функції
+//   return значення;
+// }
+// fName (...)
+
+// ! Використовується тільки після оголошення
+
+// const calcSum = function (a, b) {
+//   return a + b;
+// };
+
+// console.log("calcSum(1,2) :>> ", calcSum(1, 2));
+
+// Переписати функцію для піднесення числа до степеня в форматі Function Expression
+
+// const calcPow = function (a, b) {
+//   return a ** b;
+// };
+// console.log("calcPow(20,2) :>> ", calcPow(20, 2));
+
+// Відмінності
+
+// console.log("myPow(20,2) :>> ", myPow(20, 2));
+
+// function myPow(a, b) {
+//   return a ** b;
+// }
+
+// Значення за умовчуванням
+// !Оголошується після обовʼязкових
+
+// const value = prompt('Input number', 10) 
+// console.log('value :>> ', value);
+
+// Знайти суму двох чисел, якщо друге число не передане то вважати його 1
+
+/**
+ * Function calculates summ of two numbers
+//  * @param {number} a - First summand
+//  * @param {number} [b = 1] - Second Summand
+//  * @returns {number} Returns summ of arguments
+//  */
+// function sum2(a,b = 1) {
+//     // if (b === underfined) { b = 1; }
+//     return a+b;
+// }
+
+// console.log('sum2(2,3) :>> ', sum2(2,3));
+// console.log('sum2(2) :>> ', sum2(2));
+
+// Переписати так щоб, якщо користувач передає тільки 1 параметр, то це to
+
+// function sumFromAToB(from, to) {
+//   let sum = 0;
+//   for (let i = from; i <= to; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// console.log('sumFromAToB(1,5) :>> ', sumFromAToB(1, 5));
+// console.log('sumFromAToB(1,5) :>> ', sumFromAToB(100, 110));
+
+// Прописати JSDoc для sumFromAToB, або функції піднесеня до степеня
+/**
+ * Function for calculates the sum of given numbers
+ * @param {number} to - First summand
+ * @param {number} [from = 1] - Second summand
+ * @returns {number} Returns summ from number to number
+ */
+function sumFromAToB(to, from = 1) {
+  let sum = 0;
+  for (let i = from; i <= to; i++) {
+    sum += i;
+  }
+  return sum;
 }
+let sumto1 = sumFromAToB(10)
+console.log(sumto1);
 
-console.log('isAdult :>> ', isAdult(2));
-
+//
