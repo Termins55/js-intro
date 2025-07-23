95ac3fbf277ee0dc90e6022510eba267845905f1
Termins55 <kevgame123@gmail.com>
2025-07-23 18:57:55 +0300

    Add lexicalEnv

---
+++ assets/js/lexicalEnv.js
@@ -0,0 +1,71 @@
+//Scope / Область видимості --------------------------
+
+// Змінні, оголошені поза будь-яким блоком - глобальні, видно всім блокам (Scope - Script)
+// Змінні, оголошені усередині блоку, тобто в {} - локальні, видно тільки всередині блоку (Scope - Block/Local)
+
+const globalVar = "global";
+
+if (true) {
+  const localVarIn1If = " local in 1 if";
+  if (true) {
+    const localVarIn2If = "local in 2 if";
+    console.log("globalVar :>> ", globalVar);
+    console.log("localVarIn1If :>> ", localVarIn1If);
+    console.log("localVarIn2If :>> ", localVarIn2If);
+  }
+}
+
+// Lexical Environment / Лексичне оточення --------------------------
+
+// Лексичне оточення - концепція, згідно з якою оточення ф-ї
+// формується згідно з місцем її оголошення в коді
+
+// const a = 'global';
+
+// function f1 () {
+//   const a = 'local';
+//   f2();
+// }
+
+// function f2 () {
+//   console.log('a :>> ', a);
+// }
+
+// f1();
+
+// Closure / Замикання--------------------------
+
+// Замикання - це комбінація функції та лексичного оточення, в якому ця функція була визначена.
+// У Closure в Scope - локальні змінні функції, всередині якої функція, що виконується, була створена.
+
+// const a = 'global';
+
+// function f1 () {
+//   const a = 'local';
+
+//   function f2 () {
+//     console.log('a :>> ', a);
+//   }
+
+//   f2();
+// }
+// f1();
+
+// -----------------------------------------
+
+const a = 'global';
+
+function f1 () {
+  const a = 'local';
+
+  const f2 = function () {
+    console.log('a :>> ', a);
+  };
+
+  console.log('f2 :>> ', f2);
+
+  return f2;
+}
+
+const f2FromF1 = f1(); // Function-object + LexicalEnvironment
+f2FromF1();
