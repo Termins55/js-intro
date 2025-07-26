aa9ea6482d5e66bb8f1c9b0e0f32a2568d4bc9d4
Termins55 <kevgame123@gmail.com>
2025-07-26 19:35:03 +0300

    Add error handling ex 2

--- assets/js/errorHandling.js
+++ assets/js/errorHandling.js
@@ -12,17 +12,17 @@ try {
 //          \/
 // catch  (error)
 
-const a = 10;
-console.log('a :>> ', a);
+// const a = 10;
+// console.log("a :>> ", a);
 
-try {
-  const c = a + b; // код з потенційною помилкою
-  console.log('Hello after error');
-} catch (err) {
-  console.log('err :>> ', err);
-}
+// try {
+//   const c = a + b; // код з потенційною помилкою
+//   console.log("Hello after error");
+// } catch (err) {
+//   console.log("err :>> ", err);
+// }
 
-console.log('Hello');
+// console.log("Hello");
 
 // throw new Error('message');
 // console.log('Hello1');
@@ -30,27 +30,27 @@ console.log('Hello');
 // написати функцію для обчислення факторіала.
 // Передбачити, щоб можна будо обчислювати ! тільки для number, причому цілі додатні значення
 
-function factorial(n) {
-  if (typeof n !== 'number') {
-    throw new TypeError('n must be number');
-  }
-  if (n < 0 || !Number.isSafeInteger(n)) {
-    throw new RangeError('n must be not negative and integer value');
-  }
+// function factorial(n) {
+//   if (typeof n !== "number") {
+//     throw new TypeError("n must be number");
+//   }
+//   if (n < 0 || !Number.isSafeInteger(n)) {
+//     throw new RangeError("n must be not negative and integer value");
+//   }
 
-  if (n === 0) {
-    return 1;
-  }
-  return n * factorial(n - 1);
-}
+//   if (n === 0) {
+//     return 1;
+//   }
+//   return n * factorial(n - 1);
+// }
 
-try {
-  console.log(factorial(-10));
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   console.log(factorial(-10));
+// } catch (error) {
+//   console.log(error);
+// }
 
-console.log('after error');
+// console.log("after error");
 
 // Task: написати функцію для валідації імені користувача (4-32, string)
 // якщо значення валідне, то повернути його
@@ -59,49 +59,72 @@ console.log('after error');
 // validateName(name) => validatedName || throw error
 // isValid(name) => true || false
 
-function validateName(name) {
-  if (typeof name !== 'string') {
-    throw new TypeError('name must be string value');
-  }
+// function validateName(name) {
+//   if (typeof name !== "string") {
+//     throw new TypeError("name must be string value");
+//   }
 
-  if (name.length < 4 || name.length > 32) {
-    throw new RangeError('name must be min 4 max 32 characters length');
-  }
+//   if (name.length < 4 || name.length > 32) {
+//     throw new RangeError("name must be min 4 max 32 characters length");
+//   }
 
-  return name;
-}
+//   return name;
+// }
 
-try {
-  const validatedName = validateName('Ivo');
-  console.log(validatedName);
-} catch (error) {
-  if (error instanceof RangeError) {
-    console.log('RangeError handling');
-  } else if (error instanceof TypeError) {
-    console.log('TypeError handling');
-  } else {
-    console.log('Error handling');
-  }
-}
+// try {
+//   const validatedName = validateName("Ivo");
+//   console.log(validatedName);
+// } catch (error) {
+//   if (error instanceof RangeError) {
+//     console.log("RangeError handling");
+//   } else if (error instanceof TypeError) {
+//     console.log("TypeError handling");
+//   } else {
+//     console.log("Error handling");
+//   }
+// }
 
 // Task: Реалізувати функцію, яка приймає вік,
 // повертає вік, якщо параметр відповідає вимогам (???),
 // інакше генерує помилку
+// function validateAge(age) {
+//   if (typeof age !== 'number') {
+//     throw new TypeError('It`s not number');
+//   }
+//   if (age < 18) {
+//     throw new RangeError('You so young');
+//   }
+
+//   return age;
+// }
+
+// try {
+//   console.log(validateAge(true));
+// } catch (e) {
+//   console.log(e);
+// }
+
+// console.log('first');
+
+// Task: Реалізувати функцію, яка приймає вік,
+// повертає вік, якщо параметр відповідає вимогам (???),
+// інакше генерує помилку
+
 function validateAge(age) {
-  if (typeof age !== 'number') {
-    throw new TypeError('It`s not number');
+  if (typeof age !== "number") {
+    throw new TypeError("Input number");
   }
-  if (age < 18) {
-    throw new RangeError('You so young');
+  if (age <= 0 || age > 150) {
+    throw new RangeError("Input normal number");
   }
 
   return age;
 }
 
 try {
-  console.log(validateAge(true));
-} catch (e) {
-  console.log(e);
+  const validAge = validateAge(151);
+  console.log(validAge);
+} catch (error) {
+  console.log(error);
 }
-
 console.log('first');
