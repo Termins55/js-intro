ffcdbc5225d6a8cd8be990c22680b09b854e9f5f
Termins55 <kevgame123@gmail.com>
2025-08-09 18:01:19 +0300

    Add incapsulation + inheritance 1

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -27,72 +27,139 @@
 // класовий підхід
 // Класи - синтаксичний цукор над вбудованими в жс
 //         функціями-конструкторами і прототипами
-class User {
-  constructor(name, surname, age, isMale, email, isSubscribed) {
-    this._firstName = name;
-    this._lastName = surname;
-    this.age = age;
-    this._isMale = isMale;
-    this._email = email;
-    this._isSubscribed = isSubscribed;
-  }
-  //user2.age = 20
-  set age(value) {
-    if (typeof value !== "number") {
-      throw new TypeError("age must be number value");
-    }
-    if (value < 0 || value > 150) {
-      throw new RangeError("age must be between 0 and 150");
-    }
-    this._age = value;
-  }
-  // const age = user2.age
-  get age() {
-    return this._age;
-  }
+// class User {
+//   constructor(name, surname, age, isMale, email, isSubscribed) {
+//     this._firstName = name;
+//     this._lastName = surname;
+//     this.age = age;
+//     this._isMale = isMale;
+//     this._email = email;
+//     this._isSubscribed = isSubscribed;
+//   }
+//   //user2.age = 20
+//   set age(value) {
+//     if (typeof value !== "number") {
+//       throw new TypeError("age must be number value");
+//     }
+//     if (value < 0 || value > 150) {
+//       throw new RangeError("age must be between 0 and 150");
+//     }
+//     this._age = value;
+//   }
+//   // const age = user2.age
+//   get age() {
+//     return this._age;
+//   }
 
-  getFullName() {
-    return `${this._firstName} ${this._lastName}`;
-  }
-  // статичний метод - працює для класа, а не для його екземпляра
+//   getFullName() {
+//     return `${this._firstName} ${this._lastName}`;
+//   }
+//   // статичний метод - працює для класа, а не для його екземпляра
 
-  static isUser(obj) {
-    return obj instanceof User;
-  }
-}
+//   static isUser(obj) {
+//     return obj instanceof User;
+//   }
+// }
 
-try {
-  const user2 = new User(
-    "jack",
-    "wilsher",
-    23,
-    true,
-    "test@SpeechGrammarList.com",
-    true
-  );
-  console.log(user2.getFullName());
-  console.log("user2.age :>> ", user2.age);
-  user2.age = "10";
-} catch (err) {
-  console.log("error");
-}
+// try {
+//   const user2 = new User(
+//     "jack",
+//     "wilsher",
+//     23,
+//     true,
+//     "test@SpeechGrammarList.com",
+//     true
+//   );
+//   console.log(user2.getFullName());
+//   console.log("user2.age :>> ", user2.age);
+//   user2.age = "10";
+// } catch (err) {
+//   console.log("error");
+// }
 
 // Task: реалізувати клас для телефонів (марка, модель, колір, ціна, рік випуску)
 // реалізувати метод для розрахунку віку телефону
 // створити екземпляр класу, викликати для нього метод
 
-class Phone {
-  constructor(mark, model, color, price, year) {
-    this.mark = mark;
-    this.model = model;
-    this.color = color;
-    this.price = price;
-    this.year = year;
+// class Phone {
+//   constructor(mark, model, color, price, year) {
+//     this.mark = mark;
+//     this.model = model;
+//     this.color = color;
+//     this.price = price;
+//     this.year = year;
+//   }
+//   calcPhoneAge() {
+//     return new Date().getFullYear() - this.year;
+//   }
+// }
+
+// const iphone = new Phone("Apple", "iPhone", "black", "1000$", 2023);
+// console.log(iphone.calcPhoneAge())
+
+// Три кити ООП:
+
+// інкапсуляція - приховання логіки (особливостей реалізації)
+// спадкування  - перевикористання структури та логіки (альт. назва - спеціалізація, is a)
+// поліморфізм  - можливість через однаковий інтерфейс працювати з різними типами (в js поліморфізм підтипів)
+
+// інкапсуляція
+// class User {
+//   constructor(fullName) {
+//     [this._firstName, this._lastName] = fullName.split(' ');
+//   }
+//   set fullName(v) {
+//     [this._firstName, this._lastName] = v.split(' ');
+//   }
+//   get fullName() {
+//     return this._firstName + ' ' + this._lastName;
+//   }
+// }
+
+// const user = new User('Test Testovych');
+// user.fullName = 'Ivo Bobul';
+// console.log('user.fullName :>> ', user.fullName);
+
+// спадкування / розширення (extends)
+// батьківський/базовий клас
+
+class User {
+  constructor(name, surname, age, isMale, email) {
+    this._firstName = name;
+    this._lastName = surname;
+    this._age = age;
+    this._isMale = isMale;
+    this._email = email;
+    this._isBanned = false;
   }
-  calcPhoneAge() {
-    return new Date().getFullYear() - this.year;
+  getFullName() {
+    return `${this._firstName} ${this._lastName}`;
   }
 }
 
-const iphone = new Phone("Apple", "iPhone", "black", "1000$", 2023);
-console.log(iphone.calcPhoneAge())
+const user = new User("Test", "Testovych", 20, true, "test@gmail.com");
+
+// Модератор є Користувачем (спадкування)
+// permissions = {bann: true, readPrivate: false}
+// дочірній клас
+
+class Moderator extends User {
+  constructor(name, surname, age, isMale, email, permissions) {
+    super(name, surname, age, isMale, email); // виклик конструктора базового класу: new User ()
+    this._permissions = permissions;
+  }
+  sendMessage(user, message) {
+    console.log(
+      `Moderator ${this._firstName} send message ${message} to user ${user._firstName}`
+    );
+  }
+}
+
+const moderator = new Moderator("Mod", "Modovych", 30, false, "mod@gmail.com", {
+  bann: false,
+  readPrivate: false,
+  sendMessage: true,
+});
+
+console.log(moderator.getFullName());
+moderator.sendMessage(user, "Hello");
